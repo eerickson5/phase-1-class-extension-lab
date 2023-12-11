c0dcadd3bdc6980e4448a1f01fd2405b3680218c
Elise Erickson <elisece717@gmail.com>
2023-12-11 09:05:07 -0500

    creates classes for polygon, triangle, and square

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Your code here
+
+class Polygon{
+    constructor(sides){
+        this.sides = sides;
+    }
+
+    get countSides(){
+        return this.sides.length
+    }
+
+    get perimeter(){
+        return this.sides.reduce( (acc, side) => acc += side)
+    }
+}
+
+class Triangle extends Polygon{
+    get isValid(){
+        if(this.sides.length === 3){
+            const sortedSides = this.sides.sort((a,b) => a-b)
+            return sortedSides[0] + sortedSides[1] > sortedSides[2]
+        } else {
+            return false;
+        }
+    }
+}
+
+class Square extends Polygon{
+    get area() {
+        return this.sides[0] ** 2
+    }
+
+    get isValid(){
+        return this.sides.filter(side => side === Math.sqrt(this.area)).length === 4
+    }
+}
